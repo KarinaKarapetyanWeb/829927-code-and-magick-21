c3d4955785e37ecc3cf98755f7cf6eae8f388d17
Karina <karina_karapetyan@mail.ru>
2020-10-16 23:03:45 +0400

    модули в учебном проекте и перетаскивание

---
+++ js/util.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
+  window.util = {
+    setup: document.querySelector('.setup'),
+    isEscEvent: function (evt, action) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        evt.preventDefault();
+        action();
+      }
+    },
+    isEnterEvent: function (evt, action) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        action();
+      }
+    },
+    getRandomItem: function (array) {
+      return array[Math.floor(Math.random() * array.length)];
+    }
+  };
+})();
